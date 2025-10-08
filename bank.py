bcc6e2a460f6f2cdaff4b68fe8066b9c1202c38c
Muruganvarsha <148022049+Muruganvarsha@users.noreply.github.com>
2025-10-08 20:38:46 +0530

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,16 @@
+name=input()
+mail=input()
+mobileno=input()
+Bankname=input()
+IFSCcode=input()
+Availablebalance=input()
+print("/n Enter the bank details")
+print("Enter the name:",name)
+print("Enter the mail:",mail)
+print("Enter the mobileno:",mobileno)
+print("Enter the Bankname:",Bankname)
+print("Enter the IFSC code:",IFSCcode)
+print("Enter the Available balance:",Availablebalance)
+
+
+
