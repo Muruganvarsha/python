346ee685b27bf1c800098b20b14b60e550b0052b
Muruganvarsha <148022049+Muruganvarsha@users.noreply.github.com>
2025-10-08 20:23:29 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,14 @@
+name=input("enter the name:")
+rollno=input("enter the rollno:")
+print("\n Enter mark following subjects")
+Tamil=int(input("Tamil:"))
+English=int(input("English:"))
+Maths=int(input("Maths:"))
+Physics=int(input("Physics:"))
+Chemistry=int(input("Chemistry:"))
+Total=Tamil+English+Maths+Physics+Chemistry
+print(Total)
+percentage(Total/6)
+
+
+
