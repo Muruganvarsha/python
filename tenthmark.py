c3bc62382fc4badd55e740f3c4a7783daec7187e
Muruganvarsha <148022049+Muruganvarsha@users.noreply.github.com>
2025-10-08 19:59:08 +0530

    Add files via upload

---
+++ tenthmark.py
@@ -0,0 +1,26 @@
+name=input()
+tamil=int(input())
+english=int(input())
+maths=int(input())
+science=int(input())
+social=int(input())
+sum=tamil+english+maths+science+social
+print(sum)
+percentage=(sum/5)
+print("enter the name is:",name)
+print("enter the tamil is:",tamil)
+print("enter the english is:",english)
+print("enter the maths is:",maths)
+print("enter the science is:",science)
+print("enter the social is:",social)
+print("enter the percentge is:",percentage)
+
+
+
+
+
+
+
+            
+                       
+            
