04707a6ed200d23e0a1f9bee7e5c40bac1b2f7d1
Muruganvarsha <148022049+Muruganvarsha@users.noreply.github.com>
2025-10-08 19:40:54 +0530

    Add files via upload

---
+++ family.py
@@ -0,0 +1,21 @@
+name=input("Enter the name")
+mothername=input("Enter the mothername")
+fathername=input("Enter the fathername")
+brothername=input("Enter the brothername")
+print("name")
+print("mothername")
+print("fathername")
+print("brothername")
+
+
+
+
+
+
+                  
+
+
+
+
+
+
