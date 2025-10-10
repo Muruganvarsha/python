ec4abc3810c02ff5d2310e080b01c472db93964f
Muruganvarsha <148022049+Muruganvarsha@users.noreply.github.com>
2025-10-10 21:11:01 +0530

    Add files via upload

---
+++ num.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the value a:",))
+b=int(input("Enter the value b:",))
+sum=a+b
+dif=a-b
+product=a*b
+remainder=a%b
+floordivision=a//b
+print(sum)
+print(dif)
+print(product)
+print(remainder)
+print(floordivision)
---
+++ tripled.py
@@ -0,0 +1,5 @@
+a=int(input("Enter a value:",))
+tripled=a*3
+tripled>50
+print(tripled)
+print(tripled>50)
