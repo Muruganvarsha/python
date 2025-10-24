48a7904b17245a1375ebd4c23db17fbbd00c0d86
Muruganvarsha <148022049+Muruganvarsha@users.noreply.github.com>
2025-10-24 19:52:42 +0530

    Delete attendence.py

--- attendence.py
+++
@@ -1,8 +0,0 @@
-held=int(input("Enter the number of held:"))
-attended=int(input("Enter the number attended:"))
-percentage=(attended/held)*100
-print("attendance percentage:",percentage)
-if percentage>=75:
-    print("allowed to sit in exam")
-else:
-    print("not alowed to sit in exam")
